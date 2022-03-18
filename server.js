d0380ae2d2f072ec637cefcf69b230e3590ed2b1
RaheemToheeb <raheemtoheeb87@gmail.com>
2022-03-18 11:35:37 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require ("express");
+const res = require("express/lib/response");
+const port = process.env.PORT || 5050;
+const app = express();
+ app.use(express.json())
+
+ app.get("/",(req,re)=>{
+     res.send("My server is ready for use")
+ })
+
+ app.listen(port,()=>{
+     console.log(`I am active on port ${port}`)
+ })
